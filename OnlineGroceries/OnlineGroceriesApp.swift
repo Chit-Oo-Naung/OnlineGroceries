f723de51bc18bc9cb3008169e4b1fa58376885c7
ChitOo-Naung <chit.on@spectrumsoft-tech.com>
2024-01-29 14:47:17 +0630

    Initial Commit

---
+++ OnlineGroceries/OnlineGroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceriesApp.swift
+//  OnlineGroceries
+//
+//  Created by Cypher on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
