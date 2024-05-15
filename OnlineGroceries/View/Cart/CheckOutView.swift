5cc9be2268a3dfdaac625712ee87869445170d2e
Chit-Oo-Naung <chitoonaung.ts@gmail.com>
2024-05-15 23:18:22 +0700

    WIP checkout view

--- OnlineGroceries/View/Cart/CheckOutView.swift
+++ OnlineGroceries/View/Cart/CheckOutView.swift
@@ -8,8 +8,79 @@
 import SwiftUI
 
 struct CheckOutView: View {
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                
+                Text("Checkout")
+                    .font(.customfont(.bold, fontSize: 20))
+                    .frame(height: 46)
+                Spacer()
+                
+                Button {
+                    mode.wrappedValue.dismiss()
+                } label: {
+                    Image("close")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 25, height: 25)
+                }
+                
+            }
+            .padding(.top, 30)
+            
+            Divider()
+            
+            VStack {
+                HStack {
+                    Text("Delivery Type")
+                        .font(.customfont(.semibold, fontSize: 18))
+                        .foregroundColor(.secondaryText)
+                        .frame(height: 46)
+                    
+                    Spacer()
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("Delivery")
+                        .font(.customfont(.semibold, fontSize: 18))
+                        .foregroundColor(.secondaryText)
+                        .frame(height: 46)
+                    
+                    Spacer()
+                    
+                    Text("Delivery")
+                        .font(.customfont(.semibold, fontSize: 18))
+                        .foregroundColor(.primaryText)
+                        .frame(height: 46)
+                    
+                    Image("next")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(.primaryText)
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("Payment Type")
+                        .font(.customfont(.semibold, fontSize: 18))
+                        .foregroundColor(.secondaryText)
+                        .frame(height: 46)
+                    
+                    Spacer()
+                }
+                
+                Divider()            }
+            
+            
+        }
+        .padding(.horizontal, 20)
     }
 }
 
