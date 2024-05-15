6dab14d31b76115c9eb4337e7aa08f90e540f401
Chit-Oo-Naung <chitoonaung.ts@gmail.com>
2024-05-15 23:17:51 +0700

    WIP checkout view

---
+++ OnlineGroceries/View/Cart/CheckOutView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckOutView.swift
+//  OnlineGroceries
+//
+//  Created by Cypher on 15/05/2024.
+//
+
+import SwiftUI
+
+struct CheckOutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckOutView()
+}
