1e1904c2e7d0d89dc9431a5725d02f61b9191bac
Chit-Oo-Naung <chitoonaung.ts@gmail.com>
2024-05-20 22:25:40 +0700

    added my order model and viewmodel

---
+++ OnlineGroceries/ViewModel/MyOrdersViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyOrdersViewModel.swift
+//  OnlineGroceries
+//
+//  Created by Cypher on 20/05/2024.
+//
+
+import SwiftUI
+
+struct MyOrdersViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyOrdersViewModel()
+}
