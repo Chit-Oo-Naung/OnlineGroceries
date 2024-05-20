de5a0bc6c6f8cf4a3e5cb66600d2c8d6a431190e
Chit-Oo-Naung <chitoonaung.ts@gmail.com>
2024-05-20 21:47:02 +0700

    WIP oreder accepted view

---
+++ OnlineGroceries/View/Cart/OrderAcceptView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderAcceptView.swift
+//  OnlineGroceries
+//
+//  Created by Cypher on 20/05/2024.
+//
+
+import SwiftUI
+
+struct OrderAcceptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderAcceptView()
+}
