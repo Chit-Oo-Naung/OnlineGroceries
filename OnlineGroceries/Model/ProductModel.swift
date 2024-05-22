fb8e67541d8e6413c3389f66bf3aefa0c42d4a42
Chit-Oo-Naung <chitoonaung.ts@gmail.com>
2024-05-22 22:19:04 +0700

    added my order detail view model and WIP my order detail

--- OnlineGroceries/Model/ProductModel.swift
+++ OnlineGroceries/Model/ProductModel.swift
@@ -14,6 +14,8 @@ struct ProductModel: Identifiable, Equatable {
     var catId: Int = 0
     var brandId: Int = 0
     var typeId: Int = 0
+    var orderId: Int = 0
+    var qty: Int = 0
     var detail: String = ""
     var name: String = ""
     var unitName: String = ""
@@ -23,6 +25,8 @@ struct ProductModel: Identifiable, Equatable {
     var catName: String = ""
     var typeName: String = ""
     var offerPrice: Double?
+    var itemPrice: Double = 0.0
+    var totalPrice: Double = 0.0
     var price: Double = 0
     var startDate: Date = Date()
     var endDate: Date = Date()
@@ -34,6 +38,8 @@ struct ProductModel: Identifiable, Equatable {
         self.catId = dict.value(forKey: "cat_id") as? Int ?? 0
         self.brandId = dict.value(forKey: "brand_id") as? Int ?? 0
         self.typeId = dict.value(forKey: "type_id") as? Int ?? 0
+        self.orderId = dict.value(forKey: "order_id") as? Int ?? 0
+        self.qty = dict.value(forKey: "qty") as? Int ?? 0
         self.isFav = dict.value(forKey: "is_fav") as? Int ?? 0 == 1
         
         self.detail = dict.value(forKey: "detail") as? String ?? ""
@@ -45,6 +51,8 @@ struct ProductModel: Identifiable, Equatable {
         self.catName = dict.value(forKey: "cat_name") as? String ?? ""
         self.typeName = dict.value(forKey: "type_name") as? String ?? ""
         self.offerPrice = dict.value(forKey: "offer_price") as? Double
+        self.itemPrice = dict.value(forKey: "item_price") as? Double ?? 0
+        self.totalPrice = dict.value(forKey: "total_price") as? Double ?? 0
         self.price = dict.value(forKey: "price") as? Double ?? 0
         self.startDate = (dict.value(forKey: "start_date") as? String ?? "").stringDateToDate() ?? Date()
         self.endDate = (dict.value(forKey: "end_date") as? String ?? "").stringDateToDate() ?? Date()
--- OnlineGroceries/View/Account/MyOrderDetailView.swift
+++ OnlineGroceries/View/Account/MyOrderDetailView.swift
@@ -6,13 +6,71 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct MyOrderDetailView: View {
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @StateObject var detailVM: MyOrderDetailViewModel = MyOrderDetailViewModel(prodObj: MyOrderModel(dict: [:]))
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            ScrollView {
+                
+            }
+            
+            VStack {
+                
+                HStack {
+                    Button {
+                        mode.wrappedValue.dismiss()
+                    } label: {
+                        Image("back")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                    }
+                    
+                    Text("My Order Detail")
+                        .font(.customfont(.semibold, fontSize: 16))
+                        .foregroundColor(.primaryText)
+                    
+                    Spacer()
+                    
+                }
+                Spacer()
+            }
+            .padding(.top, .topInsets)
+            .padding(.horizontal, 20)
+        }
+        .alert(isPresented: $detailVM.showError, content: {
+            Alert(title: Text(Globs.AppName), message: Text(detailVM.errorMessage), dismissButton: .default(Text("Ok")))
+        })
+        .navigationTitle("")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarHidden(true)
+        .ignoresSafeArea()
     }
 }
 
 #Preview {
-    MyOrderDetailView()
+    MyOrderDetailView(detailVM: MyOrderDetailViewModel(prodObj: MyOrderModel(dict: [
+        "offer_price": 2.49,
+        "start_date": "2024-01-30T13:00:00.000Z",
+        "end_date": "2024-07-30T13:00:00.000Z",
+        "prod_id": 10,
+        "cat_id": 1,
+        "brand_id": 1,
+        "type_id": 1,
+        "name": "Bell Peppers",
+        "detail": "Bell peppers (Capsicum annuum) are fruits that belong to the nightshade family. They are low in calories and rich in vitamin C and other antioxidants, making them an excellent addition to a healthy diet.",
+        "unit_name": "gm",
+        "unit_value": "250",
+        "nutrition_weight": "100g",
+        "price": 1.99,
+        "image": "http://localhost:3001/img/product/20230731101409149FomkojOsMt.png",
+        "cat_name": "Frash Fruits & Vegetable",
+        "type_name": "Pulses",
+        "is_fav": 0
+    ])))
 }
