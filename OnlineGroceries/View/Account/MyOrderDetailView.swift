0240cec25aa5704934e5a18d10fd42d0fb576825
Chit-Oo-Naung <chitoonaung.ts@gmail.com>
2024-05-22 22:18:31 +0700

    added my order detail view model and WIP my order detail

---
+++ OnlineGroceries/View/Account/MyOrderDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  MyOrderDetailView.swift
+//  OnlineGroceries
+//
+//  Created by Cypher on 22/05/2024.
+//
+
+import SwiftUI
+
+struct MyOrderDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyOrderDetailView()
+}
---
+++ OnlineGroceries/ViewModel/MyOrderDetailViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MyOrderDetailViewModel.swift
+//  OnlineGroceries
+//
+//  Created by Cypher on 22/05/2024.
+//
+
+import SwiftUI
+
+struct MyOrderDetailViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyOrderDetailViewModel()
+}
